a3022750f3983cfdf9e412c5d1d0e3d178e87bfc
macurwen <macurwen@redhat.com>
2016-06-08 09:45:50 -0300

    Add support for Replica Set

--- src/main/java/org/openshift/mlbparks/mongo/DBConnection.java
+++ src/main/java/org/openshift/mlbparks/mongo/DBConnection.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.UnknownHostException;
+import java.util.List;
+import java.util.ArrayList;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
@@ -14,6 +16,7 @@ import com.mongodb.DBCollection;
 import com.mongodb.DBObject;
 import com.mongodb.Mongo;
 import com.mongodb.ReadPreference;
+import com.mongodb.ServerAddress;
 import com.mongodb.util.JSON;
 
 @Named
@@ -28,24 +31,27 @@ public class DBConnection {
 
 	@PostConstruct
 	public void afterCreate() {
-		String mongoHost = (System.getenv("MONGODB_SERVICE_HOST") == null) ? "127.0.0.1" : System.getenv("MONGODB_SERVICE_HOST");
-		String mongoPort = (System.getenv("MONGODB_SERVICE_PORT") == null) ? "27017" : System.getenv("MONGODB_SERVICE_PORT"); 
+		//String mongoHost = (System.getenv("MONGODB_SERVICE_HOST") == null) ? "127.0.0.1" : System.getenv("MONGODB_SERVICE_HOST");
+		//String mongoPort = (System.getenv("MONGODB_SERVICE_PORT") == null) ? "27017" : System.getenv("MONGODB_SERVICE_PORT"); 
 		String mongoUser = (System.getenv("MONGODB_USER")== null) ? "mlbparks" : System.getenv("MONGODB_USER");
 		String mongoPassword = (System.getenv("MONGODB_PASSWORD") == null) ? "mlbparks" : System.getenv("MONGODB_PASSWORD");
 		String mongoDBName = (System.getenv("MONGODB_DATABASE") == null) ? "mlbparks" : System.getenv("MONGODB_DATABASE");
 		// Check if we are using a mongoDB template or mongodb RHEL 7 image
+		/*
 		if (mongoHost == null) {
 			mongoHost = System.getenv("MONGODB_24_RHEL7_SERVICE_HOST");
 		} 
 		if (mongoPort == null) {
 			mongoPort = System.getenv("MONGODB_24_RHEL7_SERVICE_PORT");
 		}
-		
+		*/
 		int port = Integer.decode(mongoPort);
 		
 		Mongo mongo = null;
 		try {
-			mongo = new Mongo(mongoHost, port);
+			mongo = new Mongo(getServers(3));
+			
+			//mongo = new Mongo(mongoHost, port);
 			mongo.setReadPreference(ReadPreference.primaryPreferred());
 			System.out.println("Connected to database");
 		} catch (UnknownHostException e) {
@@ -64,6 +70,26 @@ public class DBConnection {
 
 	public DB getDB() {
 		return mongoDB;
+	}
+	
+	private List<ServerAddress> getServers(int count) {
+		List<ServerAddress> servers = new ArrayList<ServerAddress>();
+		for (int i=0;i<count;i++) {
+			String mongohost = System.getenv("REPLICA_SERVICE_"+ i +"_HOST");
+			String mongoport = System.getenv("REPLICA_SERVICE_"+ i +"_PORT");
+			if (null != mongohost && null != mongoport) {
+				ServerAddress address = new ServerAddress(mongohost, Integer.decode(mongoPort));
+				servers.add(address);
+			}
+		}
+		if(servers.isEmpty()) {
+			String mongoHost = (System.getenv("MONGODB_SERVICE_HOST") == null) ? "127.0.0.1" : System.getenv("MONGODB_SERVICE_HOST");
+			String mongoPort = (System.getenv("MONGODB_SERVICE_PORT") == null) ? "27017" : System.getenv("MONGODB_SERVICE_PORT"); 
+			if (null != mongohost && null != mongoport) {
+				ServerAddress address = new ServerAddress(mongohost, Integer.decode(mongoPort));
+				servers.add(address);
+			}
+		}
 	}
 
 	private void initDatabase(DB mongoDB) {
